4702bbbbd9e2a1825e493ef71e4a240d79c76668
szpatrik21 <szep.patrik@simonyiszki.org>
2025-02-17 14:23:19 +0100

    Switch case

--- JavaApplication5.java
+++ JavaApplication5.java
@@ -42,22 +42,91 @@ public class JavaApplication5 {
              System.out.println("A 27 nem osztható 10-zel és 3-mal maradék nélkül ");
         }
         
+        /*if()
+        {
+            
+        }
+        else{
+            if()
+            {
+                
+            }
+            else(){
+                if()
+                {
+                    
+                }
+            }
+        }*/
+        
+        double szazalek = 60.0;
+        
+        if (szazalek >= 80){
+            System.out.println("Jeles");
+        }
+        else if (szazalek >= 60 && szazalek < 80){
+            System.out.println("Jó");
+        }
+        else if ( szazalek >= 50 && szazalek < 60){
+            System.out.println("Közepes");
+        }
+        else if ( szazalek >= 40 && szazalek < 50){
+            System.out.println("Elégséges");
+        }
+        else{
+            System.out.println("Elégtelen");
+        }
+        
+        boolean res = (szazalek % 2 == 0);
+        String eredmeny = (szazalek % 2 == 0) ? "Páros" : "Pártlan";
+        System.out.println(eredmeny);
         
         
+        Scanner scanner = new Scanner(System.in);
+   
+        System.out.print("Kérem a hónap számát (1-12): ");
+        int honap = scanner.nextInt();
+       
+        switch (honap) {
+            case 1:
+                System.out.println("Január");
+                break;
+            case 2:
+                System.out.println("Február");
+                break;
+            case 3:
+                System.out.println("Március");
+                break;
+            case 4:
+                System.out.println("Április");
+                break;
+            case 5:
+                System.out.println("Május");
+                break;
+            case 6:
+                System.out.println("Június");
+                break;
+            case 7:
+                System.out.println("Július");
+                break;
+            case 8:
+                System.out.println("Augusztus");
+                break;
+            case 9:
+                System.out.println("Szeptember");
+                break;
+            case 10:
+                System.out.println("Október");
+                break;
+            case 11:
+                System.out.println("November");
+                break;
+            case 12:
+                System.out.println("December");
+                break;
+            default:
+                System.out.println("Érvénytelen szám");
         
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-    } 
-}
+        }
+    }
+}   
